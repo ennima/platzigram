9d91a4f4581948b0d8901b115a0ac3aa0210d9f6
Ennima <ennima@hotmail.com>
2017-10-06 00:18:35 -0500

    Pulblic files

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'pug');
+app.use(express.static('public'))
+
 app.get('/', function(req, res){
-	res.send("Hola Mundo!");
+	res.render('index', {});
 });
 
 app.listen(3000, function(err){
